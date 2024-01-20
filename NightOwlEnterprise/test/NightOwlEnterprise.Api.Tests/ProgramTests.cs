c4b42f8156b01f6b2bf198f362426d057c3dc2b7
Servet Seker <servet.seker@invicti.com>
2024-01-20 12:49:05 +0300

    Created Test Project

---
+++ NightOwlEnterprise/test/NightOwlEnterprise.Api.Tests/ProgramTests.cs
@@ -0,0 +1,11 @@
+namespace NightOwlEnterprise.Api.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void HelloWorld()
+        {
+            Assert.True(true);
+        }
+    }
+}
