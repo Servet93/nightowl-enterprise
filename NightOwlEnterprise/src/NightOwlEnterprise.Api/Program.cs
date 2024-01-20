f13a20805796d10abd0a85bde7901874b3f922ca
Servet Seker <servet.seker@invicti.com>
2024-01-20 15:24:13 +0300

    Add a "WorkingOnEc2" flag

--- NightOwlEnterprise/src/NightOwlEnterprise.Api/Program.cs
+++ NightOwlEnterprise/src/NightOwlEnterprise.Api/Program.cs
@@ -1,4 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+var isWorkingOnEc2 = configuration.GetValue<bool>("WorkingOnEc2");
+
+Console.WriteLine($"WorkingOnEc2 -> {isWorkingOnEc2}");
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
